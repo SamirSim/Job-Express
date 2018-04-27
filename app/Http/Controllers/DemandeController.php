4485e28e5f65b8f4db23d139043b49e7bd2c3979
abboud-biko <fe_abboud@esi.dz>
2018-04-27 05:03:29 +0100

    demendes et demendeInfo views created

--- app/Http/Controllers/DemandeController.php
+++ app/Http/Controllers/DemandeController.php
@@ -3,8 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Demande as Demande;
 
 class DemandeController extends Controller
 {
-    //
+    public function demandes(){
+    	$demandes = Demande::all();
+		return view('demandes', array('demandes' => $demandes));
+    }
+
+    public function demandeInfo($id){
+    	$demande = Demande::find($id);
+    	return view('demandeInfo', array('demande' => $demande));
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('tousLesDemandes', 'DemandeController@demandes');
+
+Route::get('demandeInfo/{id}', 'DemandeController@demandeInfo');
